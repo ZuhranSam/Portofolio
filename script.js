da6f5108d224101ca5ffadd7164d9d6dace53999
Octagram <zerorangers81@gmail.com>
2023-11-28 10:19:56 +0700

    fix: fix some bugs on like section

--- script.js
+++ script.js
@@ -8,42 +8,6 @@ githubImage.addEventListener("click", function () {
   window.open("https://github.com/ZuhranSam", "_blank");
 });
 
-// LIKE SECTION
-// Mendapatkan referensi elemen gambar, elemen teks like count, dan tombol reset
-var loveImage = document.getElementById("loveImage");
-var likeCountElement = document.getElementById("likeCount");
-var resetButton = document.getElementById("resetButton");
-
-// Mengecek apakah sudah ada data likeCount di localStorage
-var likeCount = localStorage.getItem("likeCount") || 0;
-
-// Memperbarui teks like count
-likeCountElement.textContent = likeCount + (likeCount == 1 ? " Like" : " Likes");
-
-// Menambahkan event listener untuk event 'click' pada gambar
-loveImage.addEventListener("click", function () {
-  // Menambah jumlah like
-  likeCount++;
-
-  // Memperbarui teks like count
-  likeCountElement.textContent = likeCount + (likeCount === 1 ? " Like" : " Likes");
-
-  // Menyimpan jumlah like ke localStorage
-  localStorage.setItem("likeCount", likeCount);
-});
-
-// Menambahkan event listener untuk event 'click' pada tombol reset
-resetButton.addEventListener("click", function () {
-  // Mereset jumlah like menjadi 0
-  likeCount = 0;
-
-  // Memperbarui teks like count
-  likeCountElement.textContent = "0 Likes";
-
-  // Menghapus data likeCount dari localStorage
-  localStorage.removeItem("likeCount");
-});
-
 // GITHUB FOLLOWER
 const username = "ZuhranSam";
 const clientId = "88a3795dfe15e037caff";
@@ -63,3 +27,44 @@ fetch(apiUrl)
     document.getElementById("followers-count").innerText = `Followers: ${followersCount}`;
   })
   .catch((error) => console.error("Error fetching data:", error));
+
+// Like
+var likeCountElement = document.getElementById("likeCount");
+
+// Mendapatkan referensi ke dokumen likeCount di Firestore
+var likeCountRef = db.collection("likes").doc("likeCount");
+
+// Mengecek apakah sudah ada data likeCount di Firestore
+likeCountRef.get().then(function (doc) {
+  if (doc.exists) {
+    var likeCount = doc.data().count;
+    likeCountElement.textContent = likeCount + (likeCount == 1 ? " Like" : " Likes");
+  } else {
+    // Jika dokumen belum ada, inisialisasi dengan nilai default
+    likeCountElement.textContent = "0 Likes";
+    likeCountRef.set({ count: 0 });
+  }
+});
+
+// Menambahkan event listener untuk event 'click' pada gambar
+document.getElementById("loveImage").addEventListener("click", function () {
+  // Menambah jumlah like
+  likeCountRef.get().then(function (doc) {
+    var newCount = doc.data().count + 1;
+
+    // Memperbarui teks like count
+    likeCountElement.textContent = newCount + (newCount === 1 ? " Like" : " Likes");
+
+    // Menyimpan jumlah like ke Firestore
+    likeCountRef.set({ count: newCount });
+  });
+});
+
+// Menambahkan event listener untuk event 'click' pada tombol reset
+document.getElementById("resetButton").addEventListener("click", function () {
+  // Mereset jumlah like menjadi 0
+  likeCountRef.set({ count: 0 });
+
+  // Memperbarui teks like count
+  likeCountElement.textContent = "0 Likes";
+});
